6c558959ee445f911f43b0d838ccfe8245d8e641
Stefan Schultz <info@schultzsoftware.com>
2024-04-01 18:01:34 +0200

    evaluate september 2020 data of citi bike system

--- ch02/citi_bike/hitting_the_road_with_citibike.py
+++ ch02/citi_bike/hitting_the_road_with_citibike.py
@@ -20,6 +20,42 @@ import csv
 source_file = open('202009CitibikeTripdataExample.csv', 'r')
 citibike_reader = csv.DictReader(source_file)
 
-print(citibike_reader.fieldnames)
-
 # column "User Type" is named as "usertype"
+# print(citibike_reader.fieldnames)
+
+COLUMN_USERTYPE = 'usertype'
+SUBSCRIBER = 'Subscriber'
+CUSTOMER = 'Customer'
+OTHER = 'Other'
+
+user_type_counters = {
+    'Subscriber': 0,
+    'Customer': 0,
+    'Other': 0
+}
+
+completed_rows = 0
+
+# iterate through the rows in the file
+# and count the number of rides taken by subscribers and customers
+for a_row in citibike_reader:
+    completed_rows += 1
+    
+    if a_row[COLUMN_USERTYPE] == SUBSCRIBER:
+        user_type_counters[SUBSCRIBER] += 1
+    elif a_row[COLUMN_USERTYPE] == CUSTOMER:
+        user_type_counters[CUSTOMER] += 1
+    else:
+        user_type_counters[OTHER] += 1
+
+print(f"Completed rows: {completed_rows}")
+print(f"\t- Subscribers: {user_type_counters[SUBSCRIBER]}")
+print(f"\t- Customers: {user_type_counters[CUSTOMER]}")
+print(f"\t- Other: {user_type_counters[OTHER]}")
+
+print(f"\nPercent:")
+print(f"\t- Subscribers: {round((user_type_counters[SUBSCRIBER]/completed_rows)*100, 1) if user_type_counters[SUBSCRIBER] > 0 else 0.0}%")
+print(f"\t- Customers: {round((user_type_counters[CUSTOMER]/completed_rows)*100, 1) if user_type_counters[CUSTOMER] > 0 else 0.0}%")
+print(f"\t- Other: {round((user_type_counters[OTHER]/completed_rows)*100, 1) if user_type_counters[OTHER] > 0 else 0.0}%")
+
+source_file.close()
